b31465593792d82970e7dd418860342c62d56550
lucasmzd <ilucasmzd@gmail.com>
2024-05-13 18:02:26 -0300

    Interfaces Commit

---
+++ back/src/interfaces/IUser.ts
@@ -0,0 +1,10 @@
+// interface IUser {
+//   id: number;
+//   name: string;
+//   email: string;
+//   birthdate: string;
+//   nDni: number;
+//   credencialsId: number;
+// }
+
+// export default IUser;
