acbffb670461f67d84750e1e24df506644d125eb
lucasmzd <ilucasmzd@gmail.com>
2024-05-13 17:23:58 -0300

    config seteado

---
+++ back/src/config/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm";
+import { User } from "../entities/User";
+import { Appointment } from "../entities/Appointment";
+import { Credentials } from "../entities/Credential";
+import {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER} from "./envs";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: DB_HOST,
+    port: Number(DB_PORT)|| 5432,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    // dropSchema: true,    
+    synchronize: true,
+    logging: false,
+    entities: [User, Appointment, Credentials],
+    subscribers: [],
+    migrations: [],
+})
