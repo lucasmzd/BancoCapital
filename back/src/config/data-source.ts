f5a5d9a5c142980e49034086635e019b0e09e22e
lucasmzd <ilucasmzd@gmail.com>
2024-05-14 11:20:29 -0300

    Envs Fix

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,20 +1,20 @@
 import { DataSource } from "typeorm";
-import { User } from "../entities/User";
 import { Appointment } from "../entities/Appointment";
-import { Credentials } from "../entities/Credential";
-import {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER} from "./envs";
+import { Credential } from "../entities/Credential";
+import { User } from "../entities/User";
+import { DB_HOST, DB_PORT, DB_USER , DB_PASSWORD, DB_NAME } from "./envs";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: DB_HOST,
-    port: Number(DB_PORT)|| 5432,
+    port: Number(DB_PORT),
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
     // dropSchema: true,    
     synchronize: true,
-    logging: false,
-    entities: [User, Appointment, Credentials],
+    logging: true,
+    entities: [Appointment, User, Credential],
     subscribers: [],
     migrations: [],
 })
