7c2c5e769339aa55a43c535fd72216d765a69bfd
lucasmzd <ilucasmzd@gmail.com>
2024-05-14 22:01:51 -0300

    Helpers SetUp

---
+++ front/src/helpers/misTurnos.js
@@ -0,0 +1,33 @@
+const misTurnos = [
+    {
+        id: 1,
+        date: "2024-03-01",
+        time: "09:00:00",
+        user: 1,
+        status: "active"
+    },
+    {
+        id: 2,
+        date: "2024-03-02",
+        time: "10:30:00",
+        user: 2,
+        status: "active"
+    },
+    {
+        id: 3,
+        date: "2024-03-03",
+        time: "11:45:00",
+        user: 3,
+        status: "cancelled"
+    },
+    {
+        id: 4,
+        date: "2024-03-04",
+        time: "13:15:00",
+        user: 4,
+        status: "active"
+    }
+
+];
+
+export default misTurnos;
